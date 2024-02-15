24bf3b82b3751cbd97bf2d108b378cbe07e61058
James Tepper <jamesatepper@gmail.com>
2024-02-14 20:14:23 -0500

    bitboard implemented

--- app/utils/move.py
+++ app/utils/move.py
@@ -1,115 +1,114 @@
-from utils import SQUARE_TYPE, PieceColor
-from app.chess_functionality import ChessBoard
-from app.chess_functionality import Game
-from app.chess_piece import ChessPiece
-from utils.player import Player
+# from utils import SQUARE_TYPE, PieceColor
+# from app.chess_functionality import ChessBoard
+# from app.chess_functionality import Game
+# from app.chess_piece import ChessPiece
 
-class Move:
-    def __init__(self) -> None:
-        ...
+# class Move:
+#     def __init__(self) -> None:
+#         ...
 
-    def validate(
-        self, player: Player, piece: ChessPiece, board: ChessBoard, current_square: SQUARE_TYPE
-    ):
-        curr_position = board.get_index_of_square(current_square)
-        curr_rank = curr_position["rank"]
-        curr_file = curr_position["file"]
+#     def validate(
+#         self, player: Player, piece: ChessPiece, board: ChessBoard, current_square: SQUARE_TYPE
+#     ):
+#         curr_position = board.get_index_of_square(current_square)
+#         curr_rank = curr_position["rank"]
+#         curr_file = curr_position["file"]
 
-        # Check if the path to the position is clear
-        piece_position = board.position[curr_rank][curr_file]
+#         # Check if the path to the position is clear
+#         piece_position = board.position[curr_rank][curr_file]
 
-        # Get valid move from parent method
-        unfiltered_valid_moves = piece.get_valid_moves(self, board, current_square)
+#         # Get valid move from parent method
+#         unfiltered_valid_moves = piece.get_valid_moves(self, board, current_square)
 
-        # TODO Filter out moves where the piece is pinned to the King
+#         # TODO Filter out moves where the piece is pinned to the King
 
-        valid_moves = [
-            move
-            for move in unfiltered_valid_moves
-            if self.is_path_clear(board, piece_position, move)
-            and self.not_pinned_to_king(board, piece_position)
-        ]
-        return valid_moves
+#         valid_moves = [
+#             move
+#             for move in unfiltered_valid_moves
+#             if self.is_path_clear(board, piece_position, move)
+#             and self.not_pinned_to_king(board, piece_position)
+#         ]
+#         return valid_moves
 
-    def is_check(self) -> bool:
-        ...
+#     def is_check(self) -> bool:
+#         ...
 
-    def not_pinned_to_king(self, piece: ChessPiece) -> bool:
-
+#     def not_pinned_to_king(self, piece: ChessPiece) -> bool:
 
 
-    def _check_if_legal(self) -> bool:
-        ...
-        """
-        Is king hit || will king be hit by movement of piece
 
-        """
+#     def _check_if_legal(self) -> bool:
+#         ...
+#         """
+#         Is king hit || will king be hit by movement of piece
 
-    def move_piece(self, curr_sqr: SQUARE_TYPE, new_sqr: SQUARE_TYPE):
-        # check_if_legal()
-        ...
+#         """
 
-
-# TODO Implement checker if king is directly hit by move check(color)
+#     def move_piece(self, curr_sqr: SQUARE_TYPE, new_sqr: SQUARE_TYPE):
+#         # check_if_legal()
+#         ...
 
 
-class MoveHistory:
-    def __init__(self) -> None:
-        self.history = {}
+# # TODO Implement checker if king is directly hit by move check(color)
 
 
-# General Move Legality
+# class MoveHistory:
+#     def __init__(self) -> None:
+#         self.history = {}
 
-# def get_moves(board_state, position):
+
+# # General Move Legality
+
+# # def get_moves(board_state, position):
+# #     ...
+
+
+# def get_pawn_moves(board_state, position):
 #     ...
 
 
-def get_pawn_moves(board_state, position):
-    ...
+# def get_rook_moves(board_state, position):
+#     ...
 
 
-def get_rook_moves(board_state, position):
-    ...
+# def get_knight_moves(board_state, position):
+#     ...
 
 
-def get_knight_moves(board_state, position):
-    ...
+# def get_bishop_moves(board_state, position):
+#     ...
 
 
-def get_bishop_moves(board_state, position):
-    ...
+# def get_queen_moves(board_state, position):
+#     ...
 
 
-def get_queen_moves(board_state, position):
-    ...
+# def get_king_moves(board_state, position):
+#     ...
 
 
-def get_king_moves(board_state, position):
-    ...
+# # Castling
+# def can_castle(board_state, color, side):
+#     ...
 
 
-# Castling
-def can_castle(board_state, color, side):
-    ...
+# def perform_castle(board_state, color, side):
+#     ...
 
 
-def perform_castle(board_state, color, side):
-    ...
+# # En Passant
+# def can_en_passant(board_state, from_position, to_position):
+#     ...
 
 
-# En Passant
-def can_en_passant(board_state, from_position, to_position):
-    ...
+# def perform_en_passant(board_state, from_position, to_position):
+#     ...
 
 
-def perform_en_passant(board_state, from_position, to_position):
-    ...
+# # Pawn Promotion
+# def can_promote_pawn(board_state, position):
+#     ...
 
 
-# Pawn Promotion
-def can_promote_pawn(board_state, position):
-    ...
-
-
-def promote_pawn(board_state, position, new_piece):
-    ...
+# def promote_pawn(board_state, position, new_piece):
+#     ...
